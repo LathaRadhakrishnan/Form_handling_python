8d2307bc5d8b853da3f2bae97b4696643714f091
Latha <latha@alphabsolutions.com>
2022-10-24 19:56:44 +0530

    Validating data

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def review(request):
-    return render(request,"review.html")
+    if request.method=="POST":
+       user_name= request.POST["username"]
+       if user_name =="":
+         return render(request,"review.html",{
+            "has_error": True
+             })
+
+       print(user_name)
+       return HttpResponseRedirect("/thankyou")
+
+    return render(request,"review.html",{
+        "has_error": False
+    })
+
+def thankyou(request):
+    return render(request,"thankyou.html")
